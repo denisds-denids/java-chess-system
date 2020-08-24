148777bebe8a7a2dcf18520fef13ea480d1285c9
Denis Diana da Silva <denisds@hotmail.com>
2020-08-24 20:23:22 -0300

    Saving Position Class

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,10 +3,12 @@ package boardgame;
 public class Position {
 	private int row;
 	private int column;
+	
 	public Position(int row, int column) {
 		this.row = row;
 		this.column = column;
 	}
+	
 	public int getRow() {
 		return row;
 	}
